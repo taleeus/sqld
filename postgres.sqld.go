189c8eb8a3bc173f2d3d722658ee115c059f1910
Alessandro Tagliani <alessandro.tagliani@protonmail.com>
2024-02-16 14:16:49 +0000

    feat(pg): Added PgPrepareOp()

--- postgres.sqld.go
+++ postgres.sqld.go
@@ -6,10 +6,24 @@ import (
 	"strings"
 )
 
+// PgPrepare swaps all ? placeholders with postgres ones ($1, $2...)
 func PgPrepare(query string, args []driver.Value) string {
 	for i := 1; i <= len(args); i++ {
 		query = strings.Replace(query, "?", fmt.Sprintf("$%d", i), 1)
 	}
 
 	return query
+}
+
+// PgPrepareOp applies PgPrepare() to the resulting query in the operator.
+// Use this as the last operator!
+func PgPrepareOp(op SqldFn) SqldFn {
+	return func() (string, []driver.Value, error) {
+		query, args, err := op()
+		if err != nil {
+			return "", nil, err
+		}
+
+		return PgPrepare(query, args), args, nil
+	}
 }
